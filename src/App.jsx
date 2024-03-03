05a388d6c724e1bb867ce2a410df3a83e245a1a6
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-03 12:39:38 +0000

    replaced src

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import Form from "./components/Form";
+import React, { useState } from 'react';
+import FriendCard from './components/FriendCard';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+import friendsData from './friends.json';
 
 function App() {
-  return <Form />;
+  const [friends, setFriends] = useState(friendsData);
+
+  const deleteFriend = (id) => {
+    const newFriends = friends.filter(friend => friend.id !== id);
+    setFriends(newFriends);
+  };
+
+  return (
+    <Wrapper>
+      <Title>Friends List</Title>
+      {friends.map(friend => (
+        <FriendCard
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+          deleteFriend={() => deleteFriend(friend.id)}
+        />
+      ))}
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ src/components/FriendCard/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  const handleRemoveClick = () => {
+    props.deleteFriend(props.id);
+  };
+
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Address:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span className="remove" onClick={handleRemoveClick}>𝘅</span>
+    </div>
+  );
+}
+
+export default FriendCard;
