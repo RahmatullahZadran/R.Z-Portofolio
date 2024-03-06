33a5e710dc3c813fd18000e52b9b768fbdb6697c
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-06 22:01:13 +0000

    adjusted

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './style.css';
+// import './style.css';
 
 function Popover({ selectedFriend, setIsPopoverVisible, images }) {
   const [showPopover, setShowPopover] = useState(false);
