e73e5e87cbad031d7f4b3c247184e50b0ddde6ca
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-06 14:08:41 +0000

    updated the popups

--- src/components/popover/index.jsx
+++ src/components/popover/index.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 function Popover({ selectedFriend, setIsPopoverVisible, images }) {
+  const [showPopover, setShowPopover] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopover(true);
+    }, 100);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="popovers">
+    <div className={`popovers ${showPopover ? 'show' : ''}`}>
+      <button className="close-button" onClick={() => setIsPopoverVisible(false)}>×</button>
       <h3>{selectedFriend.name}</h3>
-      {/* Display the corresponding image based on the friend's ID */}
-      <img height ={250} width={400} src={images[selectedFriend.id - 1]} alt={selectedFriend.name} />
+      <img height={250} width={400} src={images[selectedFriend.id - 1]} alt={selectedFriend.name} />
       <p>Name: {selectedFriend.name}</p>
       <p>Occupation: {selectedFriend.occupation}</p>
       <p>Location: {selectedFriend.location}</p>
-      <button onClick={() => setIsPopoverVisible(false)}>Close</button>
     </div>
   );
 }
